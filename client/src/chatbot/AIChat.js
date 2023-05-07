f0e187603130e1e2fb0bf605575079afa810521c
Abishanan Sriranjan <abishanan.sriran@gmail.com>
2023-05-07 09:01:37 -0400

    add tts

--- client/src/chatbot/AIChat.js
+++ client/src/chatbot/AIChat.js
@@ -1,9 +1,18 @@
 import '../App.css'
 import './Chat.css'
 import Typewriter from 'typewriter-effect'
+import { useEffect } from 'react';
 
 function AIChat({data, useTypewriter}) {
-    return (<div className='chat-message ai-message '>
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = data;
+
+    useEffect(() => {
+        window.speechSynthesis.speak(msg);
+    }, []);
+
+    return (<div className='chat-message ai-message '
+                >
                 <div style={{ whiteSpace: "pre-line" }} className='text-white'>
                     {
                         useTypewriter
